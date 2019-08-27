2687cfc315feb53eab8280cffbd29109f1827fc5
Emily G <aspentree79@gmail.com>
2019-08-27 20:18:54 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,12 +2,33 @@ require "yaml"
 
 def load_library(file)
  file = YAML.load_file("./lib/emoticons.yml")
-file = {
-  get_meaning: { },
-  get_emoticon: { }  }
+
+library = {
+  "get_meaning":  { },
+  "get_emoticon": { } }
+
+file.each do | meaning, value |
+  english = value[0]
+  japanese = value[1]
 end
 
 
+
+
+def load_library(emoticon_file)
+emoticons = YAML.load_file('./lib/emoticons.yml')
+
+   emoticon_lib = {'get_meaning'  => {},
+                  'get_emoticon' => {} }
+
+   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+    english = value[0]
+    japanese = value[1]
+    emoticon_lib['get_meaning'][japanese] = meaning
+    emoticon_lib['get_emoticon'][english] = japanese
+  end
+  emoticon_lib
+end
 
 
 
