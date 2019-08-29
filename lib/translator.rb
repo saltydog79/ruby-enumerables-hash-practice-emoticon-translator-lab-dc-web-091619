b9715c783476494c070c07e9cc7fb5f0d9f6dfd7
Emily G <aspentree79@gmail.com>
2019-08-29 19:41:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,41 +1,51 @@
 require "yaml"
+require "pry"
 
 def load_library(file)
  file = YAML.load_file("./lib/emoticons.yml")
 
-library = {
+result = {
   "get_meaning":  { },
   "get_emoticon": { } }
 
 file.each do | meaning, value |
   english = value[0]
   japanese = value[1]
+
+result[:get_meaning][japanese] = meaning
+  result[:get_emoticon][english] = japanese
+end
+result
 end
 
 
+def get_japanese_emoticon(file = "./lib/emoticons.yml", emoticon)
+  emoticon_hash = load_library("./lib/emoticons.yml")
+
+  # the two lines above bring back only 10 errors, but adding the following lines increases errors to 12
 
 
-def load_library(emoticon_file)
-emoticons = YAML.load_file('./lib/emoticons.yml')
-
-   emoticon_lib = {'get_meaning'  => {},
-                  'get_emoticon' => {} }
-
-   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-    english = value[0]
-    japanese = value[1]
-    emoticon_lib['get_meaning'][japanese] = meaning
-    emoticon_lib['get_emoticon'][english] = japanese
-  end
-  emoticon_lib
-end
+  # if emoticon_hash["get_emoticon"].include?(english_emoticon)
+  #     emoticon_hash["get_emoticon"][english_emoticon]
+    # else
+    #   "Sorry, that emoticon was not found"
+    # end
+    end
 
 
 
-# def get_japanese_emoticon
-#   # code goes here
-# end
-#
+
+
+# returns japanese equiv to english grinning
+# returns japanese equiv to english happy
+# returns japanese equiv to english sad
+ # if statement when emoticon not found
+
+
+
+
+
+
 # def get_english_meaning
 #   # code goes here
 # end
